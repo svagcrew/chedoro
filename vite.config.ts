6a980639b28aa8f5989ec620112462bc61aec89d
Sergei Dmitriev <iserdmi@gmail.com>
2025-04-30 13:53:32 +0400

    x

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/chedoro/' : '/'
   return {
-    plugins: [react()],
-    base: mode === 'production' ? '/chedoro/' : '/',
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            base,
+          },
+        },
+      }) as PluginOption,
+    ],
+    base,
     server: {
       host: true,
     },
